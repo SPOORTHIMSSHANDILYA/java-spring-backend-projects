711329b4db01eb0f8c2539a53b7a04f425f6aeb7
SpoorthiMS <spoorthims23@gmail.com>
2026-06-02 20:49:30 +0530

    external-api-ms-openWeatherMap apis

--- API/external-api-ms/src/main/java/com/acc/external_api_ms/exceptions/GlobalExceptionHandler.java
+++ API/external-api-ms/src/main/java/com/acc/external_api_ms/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,5 +39,12 @@ public class GlobalExceptionHandler {
                 errors.put(error.getField(), error.getDefaultMessage())
         );
         return new ErrorResponse(HttpStatus.BAD_REQUEST, "Validation failed", errors.toString());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorResponse handleHttpClientErrorException(HttpClientErrorException exception) throws Exception {
+        return new ErrorResponse(HttpStatus.NOT_FOUND,"HttpClientErrorException", exception.getMessage());
     }
 }
